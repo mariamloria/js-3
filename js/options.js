97e7a40a428b4f48b4aa654402d3df6f4254e682
Batg1rl <mariamloria12@gmail.com>
2026-03-10 12:06:49 +0400

    removed zindex--;

--- js/options.js
+++ js/options.js
@@ -5,7 +5,7 @@ function showgeorgian(){
     document.getElementById("georgian").style.display = "block";
     zindex++;
     document.getElementById("georgian").style.zIndex = zindex;
-    zindex--;
+    // zindex--;
     // document.getElementById("osetian").style.display = "none";
     // document.getElementById("kazbegian").style.display = "none";
     // document.getElementById("lezginka").style.display = "none";
@@ -15,7 +15,7 @@ function showosetian(){
     document.getElementById("osetian").style.display = "block";
     zindex++;
     document.getElementById("osetian").style.zIndex = zindex;
-    zindex--;
+    // zindex--;
     // document.getElementById("kazbegian").style.display = "none";
     // document.getElementById("georgian").style.display = "none";
     // document.getElementById("lezginka").style.display = "none";
@@ -25,7 +25,7 @@ function showkazbegian(){
     document.getElementById("kazbegian").style.display = "block";
     zindex++;
     document.getElementById("kazbegian").style.zIndex = zindex;
-    zindex--;
+    // zindex--;
     // document.getElementById("georgian").style.display = "none";
     // document.getElementById("osetian").style.display = "none";
     // document.getElementById("lezginka").style.display = "none";
@@ -35,7 +35,7 @@ function showlezginka(){
     document.getElementById("lezginka").style.display = "block";
     zindex++;
     document.getElementById("lezginka").style.zIndex = zindex;
-    zindex--;
+    // zindex--;
     // document.getElementById("georgian").style.display = "none";
     // document.getElementById("osetian").style.display = "none";
     // document.getElementById("kazbegian").style.display = "none";
